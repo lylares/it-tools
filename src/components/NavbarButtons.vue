5ea98c622f9a1d3706f088ca5c416c269f563be7
lylares <lylares@users.noreply.github.com>
2025-02-22 00:43:27 +0800

    Update NavbarButtons.vue

--- src/components/NavbarButtons.vue
+++ src/components/NavbarButtons.vue
@@ -11,7 +11,7 @@ const { isDarkTheme } = toRefs(styleStore);
     <c-button
       circle
       variant="text"
-      href="https://github.com/CorentinTh/it-tools"
+      href="https://www.lylares.com/"
       target="_blank"
       rel="noopener noreferrer"
       :aria-label="$t('home.nav.githubRepository')"
@@ -24,7 +24,7 @@ const { isDarkTheme } = toRefs(styleStore);
     <c-button
       circle
       variant="text"
-      href="https://x.com/ittoolsdottech"
+      href="https://www.lylares.com"
       rel="noopener"
       target="_blank"
       :aria-label="$t('home.nav.twitterXAccount')"
